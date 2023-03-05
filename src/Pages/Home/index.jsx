c63b11bd17b404dba700a3a51bb391984a499634
Manon <garciamanon9@gmail.com>
2023-03-05 23:01:48 +0100

    start home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { BannerHome } from "../../componants/Banner/homeBanner"
-import { Description } from "../../componants/Description/description"
+import React, { useEffect, useState } from "react";
+import { BannerHome } from "../../componants/Banner/homeBanner";
+import DescriptionPanel from "../../componants/Description/description";
 
 export function Home() {
+    const [panel, setPanel] = useState([]);
+    console.log(panel);
+    useEffect(fetchDescription, []);
+
+    function fetchDescription() {
+        fetch("descriptionPanel.json")
+            .then((res) => res.json())
+            .then((res) => setPanel(res))
+            .catch(console.error);
+    };
+
     return (
         <div>
             <BannerHome />
             <div className="home_description">
-                <Description className="introduction"/>
-                <Description className="presentation"/>
-                <Description className="quoi_d_autre"/>
+                <DescriptionPanel title={panel.title} imgUrl={panel.picture} description={panel.description}/>
+                <DescriptionPanel title={panel.title} imgUrl={panel.picture} description={panel.description}/>
             </div>
         </div>
     )
---
+++ src/componants/Description/description.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DescriptionPanel(props) {
+    console.log(props)
+    return (
+        <div className="div__presentation">
+                <h2>{props.title}</h2>
+                <div>
+                    <div className="flex__presentation">
+                        <div className="p__presentation">
+                            <p>{props.description}</p>
+                        </div>
+                        <div className="divImg__presentation">
+                            <img src={props.imgUrl} alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+};
+
+export default DescriptionPanel;
